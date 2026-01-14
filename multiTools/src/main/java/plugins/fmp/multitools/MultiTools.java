ce7db2930815c53ed619df3088801198c5475cca
Frederic Marion-Poll <marion_poll@hotmail.com>
2026-01-14 22:34:13 +0100

    clean up multiTools by adding a plugin library stub

---
+++ multiTools/src/main/java/plugins/fmp/multitools/MultiTools.java
@@ -0,0 +1,17 @@
+package plugins.fmp.multitools;
+
+import icy.plugin.abstract_.Plugin;
+import icy.plugin.interface_.PluginLibrary;
+
+/**
+ * Multi Tools Shared Library Plugin
+ * 
+ * This is a library plugin that provides shared code for multiCAFE, multiSPOTS, 
+ * and multiSPOTS96 plugins. It extends Plugin and implements PluginLibrary to 
+ * register this module as a library plugin in Icy.
+ * 
+ * @author Multi Plugins
+ */
+public class MultiTools extends Plugin implements PluginLibrary {
+	
+}
