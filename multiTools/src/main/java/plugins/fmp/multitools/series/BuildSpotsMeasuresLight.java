7c94f6f0107d08139f00f9b02d29de55e78e2342
Frederic Marion-Poll <marion_poll@hotmail.com>
2026-03-04 18:04:39 +0100

    upgrade detection and memory use (by not much)

--- multiTools/src/main/java/plugins/fmp/multitools/series/BuildSpotsMeasuresLight.java
+++ multiTools/src/main/java/plugins/fmp/multitools/series/BuildSpotsMeasuresLight.java
@@ -3,6 +3,7 @@ package plugins.fmp.multitools.series;
 import plugins.fmp.multitools.experiment.Experiment;
 import plugins.fmp.multitools.experiment.spots.Spots;
 import plugins.fmp.multitools.service.SpotLevelDetectorFromCam;
+import plugins.fmp.multitools.experiment.sequence.SequenceCamData;
 
 /**
  * Memory-light series that computes spot-based measures directly from camera
@@ -24,9 +25,19 @@ public class BuildSpotsMeasuresLight extends BuildSeries {
 		exp.load_cages_description_and_measures();
 		exp.load_spots_description_and_measures();
 
-		// Prepare camera sequence and timing as in DetectLevels (cam direct)
-		exp.getSeqCamData().loadImages();
-		exp.getFileIntervalsFromSeqCamData();
+		// Prepare camera sequence and timing in a lightweight way, similar to
+		// BuildSpotsMeasuresAdvanced: attach only the first image so ROIs and overlays
+		// have a reference frame, but do NOT load the full stack into memory.
+		SequenceCamData seqCamData = exp.getSeqCamData();
+		if (seqCamData != null && seqCamData.getSequence() == null && seqCamData.getImageLoader() != null) {
+			seqCamData.attachSequence(
+					seqCamData.getImageLoader().initSequenceFromFirstImage(seqCamData.getImagesList(true)));
+		}
+		if (seqCamData != null && seqCamData.getTimeManager().getBinDurationMs() == 0) {
+			exp.loadFileIntervalsFromSeqCamData();
+		}
+
+		// Build time limits used by SpotLevelDetectorFromCam
 		exp.build_MsTimeIntervalsArray_From_SeqCamData_FileNamesList(exp.getCamImageFirst_ms());
 		getTimeLimitsOfSequence(exp);
 
