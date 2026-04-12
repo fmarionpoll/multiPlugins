78583fdbed90cbbd92bb315c15966382e5195a0a
Frederic Marion-Poll <marion_poll@hotmail.com>
2026-04-12 18:41:35 +0200

    add buttons to edit cage infos

--- multiCAFE/src/main/java/plugins/fmp/multicafe/dlg/cages/InfosCagesTable.java
+++ multiCAFE/src/main/java/plugins/fmp/multicafe/dlg/cages/InfosCagesTable.java
@@ -33,6 +33,10 @@ public class InfosCagesTable extends JPanel {
 	private JButton copyButton = new JButton("Copy table");
 	private JButton pasteButton = new JButton("Paste");
 	private JButton duplicateAllButton = new JButton("Duplicate to all");
+	private JButton duplicatePreviousButton = new JButton("Duplicate previous");
+	private JButton duplicateNextButton = new JButton("Duplicate next");
+	private JButton duplicateForwardButton = new JButton("Duplicate forward");
+	private JButton duplicateBackwardButton = new JButton("Duplicate backward");
 	private JButton noFliesButton = new JButton("Cage 0/9: no flies");
 	private MultiCAFE parent0 = null;
 	private List<Cage> cageArrayCopy = null;
@@ -57,11 +61,15 @@ public class InfosCagesTable extends JPanel {
 		JPanel panel1 = new JPanel(flowLayout);
 		panel1.add(copyButton);
 		panel1.add(pasteButton);
+		panel1.add(noFliesButton);
 		topPanel.add(panel1);
 
 		JPanel panel2 = new JPanel(flowLayout);
 		panel2.add(duplicateAllButton);
-		panel2.add(noFliesButton);
+		panel2.add(duplicatePreviousButton);
+		panel2.add(duplicateNextButton);
+		panel2.add(duplicateForwardButton);
+		panel2.add(duplicateBackwardButton);
 		topPanel.add(panel2);
 
 		JPanel tablePanel = new JPanel();
@@ -123,34 +131,67 @@ public class InfosCagesTable extends JPanel {
 				if (exp != null) {
 					int rowIndex = tableView.getSelectedRow();
 					int columnIndex = tableView.getSelectedColumn();
-					if (rowIndex >= 0) {
-						Cage cage0 = exp.getCages().getCageList().get(rowIndex);
-						CageProperties prop0 = cage0.prop;
-						for (Cage cage : exp.getCages().getCageList()) {
-							CageProperties prop = cage.getProperties();
-							if (cage.getCageRoi2D().getName().equals(cage0.getCageRoi2D().getName()))
-								continue;
-							switch (columnIndex) {
-							case 1:
-								cage.setCageNFlies(cage0.getCageNFlies());
-								break;
-							case 2:
-								prop.setFlyStrain(prop0.getFlyStrain());
-								break;
-							case 3:
-								prop.setFlySex(prop0.getFlySex());
-								break;
-							case 4:
-								prop.setFlyAge(prop0.getFlyAge());
-								break;
-							case 5:
-								prop.setComment(prop0.getComment());
-								break;
-							default:
-								break;
-							}
-						}
-					}
+					if (rowIndex >= 0)
+						duplicateColumnFromSourceToAllOtherCages(exp, rowIndex, columnIndex);
+					cageTableModel.fireTableDataChanged();
+				}
+			}
+		});
+
+		duplicatePreviousButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				Experiment exp = (Experiment) parent0.expListComboLazy.getSelectedItem();
+				if (exp != null) {
+					int rowIndex = tableView.getSelectedRow();
+					int columnIndex = tableView.getSelectedColumn();
+					List<Cage> list = exp.getCages().getCageList();
+					if (rowIndex > 0)
+						copyColumnFromSourceToTarget(list.get(rowIndex), list.get(rowIndex - 1), columnIndex);
+					cageTableModel.fireTableDataChanged();
+				}
+			}
+		});
+
+		duplicateNextButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				Experiment exp = (Experiment) parent0.expListComboLazy.getSelectedItem();
+				if (exp != null) {
+					int rowIndex = tableView.getSelectedRow();
+					int columnIndex = tableView.getSelectedColumn();
+					List<Cage> list = exp.getCages().getCageList();
+					if (rowIndex >= 0 && rowIndex < list.size() - 1)
+						copyColumnFromSourceToTarget(list.get(rowIndex), list.get(rowIndex + 1), columnIndex);
+					cageTableModel.fireTableDataChanged();
+				}
+			}
+		});
+
+		duplicateForwardButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				Experiment exp = (Experiment) parent0.expListComboLazy.getSelectedItem();
+				if (exp != null) {
+					int rowIndex = tableView.getSelectedRow();
+					int columnIndex = tableView.getSelectedColumn();
+					if (rowIndex >= 0)
+						duplicateColumnFromSourceToCageRange(exp, rowIndex, columnIndex, rowIndex + 1,
+								exp.getCages().getCageList().size());
+					cageTableModel.fireTableDataChanged();
+				}
+			}
+		});
+
+		duplicateBackwardButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				Experiment exp = (Experiment) parent0.expListComboLazy.getSelectedItem();
+				if (exp != null) {
+					int rowIndex = tableView.getSelectedRow();
+					int columnIndex = tableView.getSelectedColumn();
+					if (rowIndex >= 0)
+						duplicateColumnFromSourceToCageRange(exp, rowIndex, columnIndex, 0, rowIndex);
 					cageTableModel.fireTableDataChanged();
 				}
 			}
@@ -167,6 +208,48 @@ public class InfosCagesTable extends JPanel {
 			}
 		});
 
+	}
+
+	private void copyColumnFromSourceToTarget(Cage target, Cage source, int columnIndex) {
+		CageProperties prop = target.getProperties();
+		CageProperties prop0 = source.prop;
+		switch (columnIndex) {
+		case 1:
+			target.setCageNFlies(source.getCageNFlies());
+			break;
+		case 2:
+			prop.setFlyStrain(prop0.getFlyStrain());
+			break;
+		case 3:
+			prop.setFlySex(prop0.getFlySex());
+			break;
+		case 4:
+			prop.setFlyAge(prop0.getFlyAge());
+			break;
+		case 5:
+			prop.setComment(prop0.getComment());
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void duplicateColumnFromSourceToAllOtherCages(Experiment exp, int sourceRowIndex, int columnIndex) {
+		List<Cage> list = exp.getCages().getCageList();
+		Cage sourceCage = list.get(sourceRowIndex);
+		for (Cage cage : list) {
+			if (cage.getCageRoi2D().getName().equals(sourceCage.getCageRoi2D().getName()))
+				continue;
+			copyColumnFromSourceToTarget(cage, sourceCage, columnIndex);
+		}
+	}
+
+	private void duplicateColumnFromSourceToCageRange(Experiment exp, int sourceRowIndex, int columnIndex,
+			int fromIndexInclusive, int toIndexExclusive) {
+		List<Cage> list = exp.getCages().getCageList();
+		Cage sourceCage = list.get(sourceRowIndex);
+		for (int i = fromIndexInclusive; i < toIndexExclusive; i++)
+			copyColumnFromSourceToTarget(list.get(i), sourceCage, columnIndex);
 	}
 
 	void close() {
