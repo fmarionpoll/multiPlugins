0249334ecdd24f2bf3ae9e069659a93a4395dc8e
Frederic Marion-Poll <marion_poll@hotmail.com>
2026-05-28 11:48:19 +0200

    update minor error

--- multiSPOTS/src/main/java/plugins/fmp/multiSPOTS/dlg/kymograph/KymoOverlayFrame.java
+++ multiSPOTS/src/main/java/plugins/fmp/multiSPOTS/dlg/kymograph/KymoOverlayFrame.java
@@ -6,12 +6,12 @@ import java.awt.FlowLayout;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.util.List;
 import java.util.prefs.Preferences;
 
-import javax.swing.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
@@ -57,6 +57,7 @@ public class KymoOverlayFrame {
 	private String baseTitle;
 	private Experiment lastExperiment;
 	private ResultsOptions lastOptions;
+
 	public interface SelectedSpotsProvider {
 		List<Spot> getSelectedSpots();
 	}
@@ -213,8 +214,8 @@ public class KymoOverlayFrame {
 			return;
 		}
 		if (options.resultType == EnumResults.AGG_GREENHEIGHT_CONSO) {
-			options.spotAggregateGlobalKeyOrder = CageSpotStimulusAggregation
-					.globalStimulusConcKeysFirstSeenOrder(exp, exp.getSpots());
+			options.spotAggregateGlobalKeyOrder = CageSpotStimulusAggregation.globalStimulusConcKeysFirstSeenOrder(exp,
+					exp.getSpots());
 		} else {
 			options.spotAggregateGlobalKeyOrder = null;
 		}
